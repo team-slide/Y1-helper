5ce7c47dd5be76f60d29f82fbc098e7a9521b135
team-slide <itsryanspecter@gmail.com>
2025-08-15 00:08:50 +0100

    Update version to 1.0.0

--- y1_helper.py
+++ y1_helper.py
@@ -208,7 +208,7 @@ class Y1HelperApp(tk.Tk):
         self.download_and_unpack_config()
         
         # Version information
-        self.version = "0.9.991"
+        self.version = "1.0.0"
         
         # Backup current y1_helper.py to .old directory at launch
         self.backup_current_version()
